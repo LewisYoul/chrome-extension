1fc7a08f3d2be4d9789c8e06aefda27dc321af9e
Lewis <lewisyoul@gmail.com>
2018-02-01 16:10:55 +0000

    selected word cannot be greater than 10 characters

--- spec/ContentSpec.js
+++ spec/ContentSpec.js
@@ -11,5 +11,17 @@ describe("App", function() {
     it("returns the longest word length", function() {
       expect(app.returnLongestWord("one,two three'4;five-duckling").length).toEqual(8)
     });
+    it("doesn't return a word that is longer than 10 characters", function() {
+      expect(app.returnLongestWord("one,two three'4;five-duckling,sweetbabyjesus").word).toEqual("duckling")
+    });
+  });
+
+  describe("#splitWordsByCapital", function() {
+    it("returns 'the' as the first word", function() {
+      expect(app.splitWordsByCapital(["theCat"])[0]).toEqual("the")
+    });
+    it("returns 'Buns' as the third word", function() {
+      expect(app.splitWordsByCapital(["theCatBunsRabbitVulture"])[2]).toEqual("Buns")
+    });
   });
 });
--- src/Content.js
+++ src/Content.js
@@ -2,14 +2,23 @@ function App() {
 }
 
 App.prototype.returnLongestWord = function(string) {
-  var words = string.split(/[^a-zA-Z']/)
+  var words = string.split(/[^a-zA-Z']/);
+  words = this.splitWordsByCapital(words)
   var longestWord = { word: "", length: 0 }
 
   for (var i = 0; i < words.length; i++) {
-    if (words[i].length > longestWord.length) {
+    if (words[i].length > longestWord.length && (words[i].length < 10)) {
       longestWord.word = words[i]
       longestWord.length = words[i].length
     }
   }
   return longestWord
+}
+
+App.prototype.splitWordsByCapital = function(wordsArray) {
+  var finalArray = []
+  wordsArray.forEach(function(word) {
+    finalArray = word.split(/(?=[A-Z])/).concat(finalArray)
+  });
+  return finalArray
 }
