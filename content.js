8b058996a91873b6e3e9016bf17f1fc57ff80b95
Lewis <lewisyoul@gmail.com>
2018-02-01 12:38:49 +0000

    longestWord now returns the length of the longest word on page

--- content.js
+++ content.js
@@ -1,1 +1,22 @@
 console.log(document.body.innerText.split(/[^a-zA-Z']/))
+
+var words = document.body.innerText.split(/[^a-zA-Z']/)
+
+function longestWord(string) {
+
+  var words = string.split(/[^a-zA-Z']/)
+  console.log(words.length)
+  var longest = 0
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > longest) {
+      longest = words[i].length
+      console.log(longest)
+    }
+  }
+  console.log(longest)
+  return longest
+}
+
+
+longestWord(document.body.innerText)
