814828c6640782d9a9535f0bc246bb14d468e03b
Lewis <lewisyoul@gmail.com>
2018-02-01 15:20:27 +0000

    Successful API call to OXford dictionary

--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,12 @@
-returnLongestWord(document.body.innerText)
+var app = new App()
+
+var word = app.returnLongestWord(document.body.innerText)
+
+$.ajax({
+  url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/sausage",
+  beforeSend: function(xhr){
+    xhr.setRequestHeader('app_id', '8aedcaf1')
+    xhr.setRequestHeader('app_key', '631529d8a03ed59b7f9944f6a476bf21')
+  },
+  success: function(data) { console.log(data) }
+});
