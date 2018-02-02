c52a8414bb9a39f2a72b9b796f3ad1c97da10453
Lewis <lewisyoul@gmail.com>
2018-02-02 18:16:45 +0000

    extract get resquest into stand alone app method

--- src/Content.js
+++ src/Content.js
@@ -1,29 +1,24 @@
 function App() {
 }
 
-App.prototype.returnLongestWord = function(string) {
-  var words = this.splitWordsByCapital(this._splitStringByPunct(string))
-  var longestWord = { word: "", length: 0 }
-
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length > longestWord.length && (words[i].length < 10)) {
-      longestWord.word = words[i]
-      longestWord.length = words[i].length
+App.prototype.sendGetRequest = function(selectedWord) {
+  $.ajax({
+    url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/" + selectedWord,
+    beforeSend: function(xhr){
+      xhr.setRequestHeader('app_id', '8aedcaf1')
+      xhr.setRequestHeader('app_key', '631529d8a03ed59b7f9944f6a476bf21')
+    },
+    error: function(err) {
+      console.log(err)
+      $('#word-fact-div').remove()
+      $('body').prepend('<div id="word-fact-div" class="word">The dictionary does not know that word. Please try again.</div>')
+    },
+    success: function(data) {
+      console.log(data)
+      $('#word-fact-div').remove()
+      $('body').prepend('<div id="word-fact-div" class="word"><h1 class="word-title">'+ data.results["0"].word + '</h1><p class="word-description">' + data.results["0"].lexicalEntries["0"].entries["0"].senses["0"].definitions["0"] + '</p></div>')
     }
-  }
-  return longestWord
-}
-
-App.prototype.splitWordsByCapital = function(wordsArray) {
-  var finalArray = []
-  wordsArray.forEach(function(word) {
-    finalArray = word.split(/(?=[A-Z])/).concat(finalArray)
   });
-  return finalArray
-}
-
-App.prototype._splitStringByPunct = function(string) {
-  return string.split(/[^a-zA-Z']/)
 }
 
 App.prototype.isAlphabetical = function(string) {
--- src/Index.js
+++ src/Index.js
@@ -1,24 +1,8 @@
+var app = new App()
+
 document.addEventListener("mouseup", function() {
-  selected = document.getSelection().toString().toLowerCase()
-  console.log(selected.length)
-  console.log(document.getSelection())
-  if (selected.length != 0) {
-    $.ajax({
-      url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/" + selected,
-      beforeSend: function(xhr){
-        xhr.setRequestHeader('app_id', '8aedcaf1')
-        xhr.setRequestHeader('app_key', '631529d8a03ed59b7f9944f6a476bf21')
-      },
-      error: function(err) {
-        console.log(err)
-        $('#word-fact-div').remove()
-        $('body').prepend('<div id="word-fact-div" class="word">The dictionary does not know that word. Please try again.</div>')
-      },
-      success: function(data) {
-        console.log(data)
-        $('#word-fact-div').remove()
-        $('body').prepend('<div id="word-fact-div" class="word">'+ data.results["0"].word + ' - ' + data.results["0"].lexicalEntries["0"].entries["0"].senses["0"].definitions["0"] + '</div>')
-      }
-    });
+  selectedWord = document.getSelection().toString().toLowerCase()
+  if (app.isAlphabetical(selectedWord)) {
+    app.sendGetRequest(selectedWord)
   }
 });
