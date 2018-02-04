a505d8bfb75219ecdb4830e42e8e3b81a2c4ad3b
Lewis <lewisyoul@gmail.com>
2018-02-04 13:43:56 +0000

    Import font awesome and use right arrow

--- src/AppView.js
+++ src/AppView.js
@@ -6,5 +6,5 @@ AppView.prototype.returnWordNotFound = function() {
 }
 
 AppView.prototype.returnWordInElements = function(entry) {
-  return '<div id="word-div"><h1 class="word-title">'+ entry.word + '</h1><p class="word-lexicon">' + entry.lexicalCategory + '</p><p class="word-description">' + entry.definition + '</p><p id="next">NEXT</p></div>'
+  return '<div id="word-div"><h1 class="word-title">'+ entry.word + '</h1><p class="word-lexicon">' + entry.lexicalCategory + '</p><p class="word-description">' + entry.definition + '</p><div id="next"><i id="next" class="fas fa-arrow-right"></i></div></div>'
 }
