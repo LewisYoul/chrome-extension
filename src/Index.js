fc7a8f1fa0eaeda73c6d240c3c2944e186e63415
Lewis <lewisyoul@gmail.com>
2018-02-12 13:14:19 +0000

    remove console.logs

--- src/Index.js
+++ src/Index.js
@@ -5,7 +5,6 @@ document.addEventListener("mouseup", function() {
   selectedWord = document.getSelection().toString().toLowerCase()
   if (app.isAlphabetical(selectedWord)) {
     $.when(app.sendGetRequest(selectedWord)).done(function(data) {
-      console.log(data)
       $('#word-fact-div').remove()
       $('body').prepend('<div id="word-fact-div" class="word"></div>')
       var allEntries = data.results["0"].lexicalEntries.map(function(entry) {
@@ -17,11 +16,8 @@ document.addEventListener("mouseup", function() {
       });
       app.allEntries = allEntries
       $('#word-fact-div').prepend(app.nextWordEntry(app.allEntries))
-
       $('#next').click(removeAndAdd);
-
       $(document.body).on('click', '#next', function() {
-        console.log("BOSHTY")
       });
     });
   }
@@ -32,5 +28,4 @@ function removeAndAdd() {
   $('#word-div').remove()
   $('#word-fact-div').prepend(app.nextWordEntry(app.allEntries))
   $('#next').click(removeAndAdd)
-  console.log("Im the counter", counter)
 }
